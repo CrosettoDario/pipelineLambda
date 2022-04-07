fab9c6f203c290d512582b531e53737abb40e854
Roberto Candela <roberto.candela@itauros.com>
2022-04-07 16:27:13 +0200

    testing

---
+++ testPipeline/testS3Upload.groovy
@@ -0,0 +1,18 @@
+stage("Upload"){
+
+parameters {
+    text( name: 'bucket')
+    text( name: 'TAG_NAME')
+    text( name: 'region' )
+
+}
+        def aws_credential = 'aws-test'
+    
+        steps{
+                withAWS(region:"${region}", credentials:"${aws_credential}){
+
+                    s3Download(file:"${TAG_NAME}", bucket:"${bucket}", path:"")
+                }
+        }
+
+}
